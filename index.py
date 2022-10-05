4910e22c129c1c4f423c45c20ceb75a86a083be7
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-10-04 20:17:46 -0500

    testing deployment

--- index.py
+++ index.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
-import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 from videoGames import vgRouter
 
 # Defining our Server
 server = FastAPI()
+
+origins = ['*']
+
+server.add_middleware(
+    CORSMiddleware, 
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ['*'],
+    allow_headers = ['*'],
+)
 
 @server.get('/')
 def welcomeUser():
